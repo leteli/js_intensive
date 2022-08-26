7a31726eccf1df66d2a6fdccfc7e683ce404fca2
Nastia Kartseva <danetzachem@gmail.com>
2022-08-27 02:03:40 +0300

    Added comment on algorithm complexity

--- queue.js
+++ queue.js
@@ -4,26 +4,26 @@ class Queue {
   }
 
   size() {
-    return this.queue.length;
+    return this.queue.length; // O(1)
   }
 
   isEmpty() {
-    return this.size() === 0;
+    return this.size() === 0; // O(1)
   }
 
   enqueue(value) {
-    this.queue.push(value);
+    this.queue.push(value); // O(1)
   }
 
   dequeue() {
     if (!this.isEmpty()) {
-      return this.queue.shift();
+      return this.queue.shift(); // Cложность O(n) - требует новой нумерации индексов с нуля!
     }
   }
 
   front() {
     if (!this.isEmpty()) {
-      return this.queue[0];
+      return this.queue[0]; // O(1)
     }
   }
 }
