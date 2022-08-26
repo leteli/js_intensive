9e9c2c57bbd322aa30c3089e4fb46b00edc83f31
Nastia Kartseva <danetzachem@gmail.com>
2022-08-27 01:58:02 +0300

    Added comment on algorithm complexity

--- stack.js
+++ stack.js
@@ -4,28 +4,28 @@ class Stack {
   }
 
   length() {
-    return this.stack.length;
+    return this.stack.length; // Сложность O(1) - одна операция независимо от размера массива
   }
 
   isEmpty() {
-    return this.length() === 0;
+    return this.length() === 0; // Сложность O(1) 
   }
 
   push(value) {
-    this.stack.push(value);
+    this.stack.push(value); // Сложность O(1)
   }
 
   pop() {
     if (this.isEmpty()) {
       return;
     }
-    return this.stack.pop();
+    return this.stack.pop(); // Сложность O(1)
   }
 
   peek() {
     if (this.isEmpty()) {
       return;
     }
-    return this.stack[this.length() - 1];
+    return this.stack[this.length() - 1]; // Сложность O(1)
   }
 }
