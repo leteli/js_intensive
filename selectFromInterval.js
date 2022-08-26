b165713f5d8021ef651348e2c58a61923205a9e7
Nastia Kartseva <danetzachem@gmail.com>
2022-08-26 17:46:19 +0300

    Added selectFromInterval function

---
+++ selectFromInterval.js
@@ -0,0 +1,19 @@
+const isValidNumber = (value) => typeof value === 'number' && !isNaN(value);
+
+const selectFromInterval = (arr, limit1, limit2) => {
+  if (!isValidNumber(limit1) || !isValidNumber(limit2)) {
+    throw new Error('Границы интервала должны быть числом!');
+  }
+  if (!Array.isArray(arr) ||
+      arr.some((el) => !isValidNumber(el)) ||
+      arr.length === 0) {
+    throw new Error('Первый параметр должен быть массивом чисел!');
+  }
+  return arr.filter((el) => {
+    if (limit1 < limit2) {
+      return el >= limit1 && el <= limit2;
+    } else {
+      return el >= limit2 && el <= limit1;
+    }
+  });
+};
